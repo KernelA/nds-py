69a528420eb965afe256d71c51adbc92def272d2
KernelA <KernelA@users.noreply.github.com>
2018-09-16 18:37:16 +0300

    Added setuptools support instead distutils

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name="nds",
       version="0.2.0",
       description="Non-dominated sorting algorithm.",
-      author= "Alexander Kryuchkov",
+      author="Alexander Kryuchkov",
       author_email= "KernelA@users.noreply.github.com",
-      url= "https://github.com/KernelA/nds-py",
-      long_description = "See Buzdalov M., Shalyto A. A Provably Asymptotically Fast Version of the Generalized Jensen Algorithm for Non-dominated Sorting // Parallel Problem Solving from Nature XIII.- 2015. - P. 528-537. - (Lecture Notes on Computer Science ; 8672).",
-      license = "MIT",
-      packages = ["nds"]
-     )
+      url="https://github.com/KernelA/nds-py",
+      license="MIT",
+      long_description="See Buzdalov M., Shalyto A. "
+                       "A Provably Asymptotically Fast Version of the Generalized Jensen Algorithm for Non-dominated Sorting //"
+                       " Parallel Problem Solving from Nature XIII.- 2015. - P. 528-537. - (Lecture Notes on Computer Science ; 8672).",
+      packages=["nds"],
+      python_requires=">=3.5",
+      classifiers=[
+            "Development Status :: 4 - Beta"
+      ]
+      )
 
