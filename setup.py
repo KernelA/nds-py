ffe46455b10f3b95be07edbb4305d5162fbbcaba
KernelA <KernelA@users.noreply.github.com>
2023-02-21 23:00:22 +0300

    Store version in the setup.cfg

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 from setuptools import setup
 
-with open("version.txt", encoding="utf-8") as file:
-    version = file.readlines()[0].strip()
-
-if not version:
-    raise ValueError("Found empty version")
-
 setup(name="nds",
-      version=version,
-      license="MIT",
       packages=["nds"],
       keywords=["nds", "multiobjective-optimization", "non-dominated-sorting"],
       python_requires=">=3.6"
